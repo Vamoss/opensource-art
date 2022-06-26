ff8a6603e25f83cf8a2b33510106ec99fadf2911
amarocolas <amaroles@gmail.com>
2022-06-26 19:09:18 +0200

    add SideBar component

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,7 +1,43 @@
 import React from "react";
+import { useFileSystem } from "../hooks/useFileSystemState";
 
 const SideBar = () => {
-  return <div>sidebar</div>;
+  const { files, code, updateCode, runSketch, loadFile, saveFile } =
+    useFileSystem();
+
+  return (
+    <nav>
+      <button onClick={runSketch}>Rodar Sketch</button>
+      <button onClick={saveFile}>Salvar Sketch</button>
+      <h2>files</h2>
+      {files.initial.map((fileName) => (
+        <button
+          key={fileName}
+          onClick={() =>
+            loadFile({
+              dir: "initial",
+              name: fileName,
+            })
+          }
+        >
+          {fileName}
+        </button>
+      ))}
+      {files.derived.map((fileName) => (
+        <button
+          key={fileName}
+          onClick={() =>
+            loadFile({
+              dir: "derived",
+              name: fileName,
+            })
+          }
+        >
+          {fileName}
+        </button>
+      ))}
+    </nav>
+  );
 };
 
 export default SideBar;
--- src/pages/CodeEditor.js
+++ src/pages/CodeEditor.js
@@ -1,56 +1,18 @@
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { useFileSystem } from "../hooks/useFileSystemState";
+import SideBar from "../components/SideBar";
 
 const CodeEditor = () => {
-  const { files, code, updateCode, runSketch, loadFile, saveFile } =
-    useFileSystem();
+  const { code, updateCode } = useFileSystem();
 
   const onChange = (value) => {
     updateCode(value);
   };
 
-  const updateFile = () => {
-    window.ipcRenderer.send("app:update-file", {
-      name: "testfile.js",
-      content: code,
-    });
-  };
-
   return (
     <main className="container">
-      <nav>
-        <button onClick={runSketch}>Rodar Sketch</button>
-        <button onClick={saveFile}>Salvar Sketch</button>
-        <button onClick={updateFile}>Atualizar Sketch</button>
-        <h2>files</h2>
-        {files.initial.map((fileName) => (
-          <button
-            key={fileName}
-            onClick={() =>
-              loadFile({
-                dir: "initial",
-                name: fileName,
-              })
-            }
-          >
-            {fileName}
-          </button>
-        ))}
-        {files.derived.map((fileName) => (
-          <button
-            key={fileName}
-            onClick={() =>
-              loadFile({
-                dir: "derived",
-                name: fileName,
-              })
-            }
-          >
-            {fileName}
-          </button>
-        ))}
-      </nav>
+      <SideBar />
       <section>
         <CodeMirror
           value={code}
