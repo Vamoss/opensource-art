499a2434de67173ffd0ba427dae461afc9c6bf16
amarocolas <amaroles@gmail.com>
2022-07-06 20:39:27 +0200

    add window reload and vamoss sketch as initial

--- public/main/data/derived/71503bbd-7b7b-4eb0-a787-bf75a6265a65/sketch.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
-  fill(100, 150, 230);
-  rect(mouseX, mouseY, 80, 80);
-}
--- public/main/data/derived/9b672a94-9626-4a2c-b546-c74418ad4659/sketch.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
-  fill(0);
-  ellipse(mouseX, mouseY, 80, 80);
-}
