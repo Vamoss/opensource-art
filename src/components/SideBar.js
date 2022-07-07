835814b6da1eba9dc130bb4079b41eb4a0fd8111
amarocolas <amaroles@gmail.com>
2022-07-07 22:34:01 +0200

    add a bit of styling

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -4,26 +4,26 @@ import { useFileSystem } from "../hooks/useFileSystemState";
 import styles from "./SideBar.module.css";
 
 const SideBar = () => {
-  const { files, runSketch, loadFile, saveFile } = useFileSystem();
+  const { activeSketch, runSketch, saveFile } = useFileSystem();
 
   return (
     <nav className={styles.sidebar}>
-      <h2 className={styles.title}>Navegação</h2>=
-      <Link to="/" className={styles.link}>
-        Código
+      <h2 className={styles.title}>Nav</h2>
+      <Link to="/" className={styles.button}>
+        <img src="/assets/code-svgrepo-com.svg" alt="code" />
       </Link>
-      <Link to="/sketches" className={styles.link}>
+      {/* <Link to="/sketches" className={styles.link}>
         Sketches
-      </Link>
-      <Link to="/force-graph" className={styles.link}>
-        Gráfico
+      </Link> */}
+      <Link to="/force-graph" className={styles.button}>
+        <img src="/assets/network-svgrepo-com.svg" alt="network graph" />
       </Link>
       <h2 className={styles.title}>Ações</h2>
       <button className={styles.button} onClick={runSketch}>
-        Rodar Sketch
+        <img src="/assets/play-svgrepo-com.svg" alt="rodar sketch" />
       </button>
       <button className={styles.button} onClick={saveFile}>
-        Salvar Sketch
+        <img src="/assets/save-svgrepo-com.svg" alt="salvar sketch" />
       </button>
     </nav>
   );
