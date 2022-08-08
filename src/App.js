d714be8cb4b5bd16dea418e0ef9e2ce60336339d
amarocolas <amaroles@gmail.com>
2022-08-08 22:10:53 +0200

    navega pro grafico quando salva

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FileSystemProvider } from "./hooks/useFileSystemState";
 import CodeEditor from "./pages/CodeEditor";
-import ForceGraphPage from "./pages/ForceGraphPage";
 import MeatBallsPage from "./pages/MeatBallsPage";
 import Sketches from "./pages/Sketches";
 import Viewer from "./pages/Viewer";
